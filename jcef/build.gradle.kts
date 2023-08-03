08c52bca63372245075bb43ffc6ebe80c59cdd18
xietianduo <>
2023-08-03 11:49:33 +0800

    add jitpack

--- jcef/build.gradle.kts
+++ jcef/build.gradle.kts
@@ -1,8 +1,18 @@
 plugins {
+    `java-library`
     `common-jvm`
+    `maven-publish`
 }
 
 dependencies {
     jvmMainApi(libs.kotlinx.coroutines.core)
     jvmMainApi(libs.jcefmaven)
+}
+publishing {
+    publications {
+        create<MavenPublication>("webview") {
+            groupId = "com.goldenduo.compose.desktop"
+            from(components["java"])
+        }
+    }
 }
