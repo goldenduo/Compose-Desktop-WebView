fe2db711bda00a82847cc6c2171a4819ba748792
xietianduo <>
2023-08-03 14:12:50 +0800

    fix jitpack

--- jcef/build.gradle.kts
+++ jcef/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     `maven-publish`
 }
 group = "com.goldenduo.compose.desktop"
-version ="0.0.1"
+
 dependencies {
     jvmMainApi(libs.kotlinx.coroutines.core)
     jvmMainApi(libs.jcefmaven)
